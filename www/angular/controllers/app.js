25ed06adff0128a70b53dc9e3d23502fdf63e9ff
S Vinesh <svinesh3691@gmail.com>
2016-08-10 09:30:27 +0530

    sdfsdf

--- www/angular/controllers/app.js
+++ www/angular/controllers/app.js
@@ -9,7 +9,50 @@ app.controller('app', ['$scope','seven','$state','webServices','fns','C','$http'
 
 
 
-            var TpushService = {};
+
+            
+
+            
+}]);
+
+// Home Controller
+app.controller('news', ['$scope','fns','seven','$state','webServices','C','newsFactory',
+    function ( $scope , fns , seven , $state, webServices, C , newsFactory) {
+            seven.hideIndicator();
+            $scope.newses = [];
+            $scope.api_base_url = C.api_base_url;
+            var tillNow = localStorage.tillNow || 0;
+            var populateNews = function() { 
+                $scope.newses = newsFactory.theNewsArray;
+            }
+            var fetchNews = function() {
+                webServices.master('api/fetchNews',{
+                            'tillNow'       : tillNow
+                }).then(function(res){
+                    if(res.data.news) {
+                            console.log(res.data.news.length);
+                            localStorage.tillNow = res.data.news[res.data.news.length-1].news_id;
+
+                            for (var i = 0 ; i < res.data.news.length; i++) {
+                                        console.log(res.data.news[i]);
+                                        fns.query('INSERT into news_main (news_id,news_title,news_body,news_image,news_type,news_add_date) VALUES (?,?,?,?,?,?)', [res.data.news[i].news_id,res.data.news[i].news_title,res.data.news[i].news_body,res.data.news[i].news_image,res.data.news[i].news_type,res.data.news[i].news_add_date],function(res){
+                                            console.log(res);
+                                        });
+                            };
+                            populateNews();
+                    } else {
+                        // populateNews();
+                    }
+                    
+                });
+            }
+            populateNews();
+            fetchNews();
+
+
+
+
+             var TpushService = {};
             TpushService.push = function() {
                         console.log('Initializing Push...');
                         
@@ -74,44 +117,9 @@ app.controller('app', ['$scope','seven','$state','webServices','fns','C','$http'
                     new TpushService.push();
                 },3000)
             }
-            
+
 
             
-}]);
-
-// Home Controller
-app.controller('news', ['$scope','fns','seven','$state','webServices','C','newsFactory',
-    function ( $scope , fns , seven , $state, webServices, C , newsFactory) {
-            seven.hideIndicator();
-            $scope.newses = [];
-            $scope.api_base_url = C.api_base_url;
-            var tillNow = localStorage.tillNow || 0;
-            var populateNews = function() { 
-                $scope.newses = newsFactory.theNewsArray;
-            }
-            var fetchNews = function() {
-                webServices.master('api/fetchNews',{
-                            'tillNow'       : tillNow
-                }).then(function(res){
-                    if(res.data.news) {
-                            console.log(res.data.news.length);
-                            localStorage.tillNow = res.data.news[res.data.news.length-1].news_id;
-
-                            for (var i = 0 ; i < res.data.news.length; i++) {
-                                        console.log(res.data.news[i]);
-                                        fns.query('INSERT into news_main (news_id,news_title,news_body,news_image,news_type,news_add_date) VALUES (?,?,?,?,?,?)', [res.data.news[i].news_id,res.data.news[i].news_title,res.data.news[i].news_body,res.data.news[i].news_image,res.data.news[i].news_type,res.data.news[i].news_add_date],function(res){
-                                            console.log(res);
-                                        });
-                            };
-                            populateNews();
-                    } else {
-                        // populateNews();
-                    }
-                    
-                });
-            }
-            populateNews();
-            fetchNews();
 }]);
 
 
