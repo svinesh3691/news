40cb2c631968f0fea00e2d466fee8a9c2b877e14
S Vinesh <svinesh3691@gmail.com>
2016-08-10 08:54:38 +0530

    Ha ha

--- www/angular/app.js
+++ www/angular/app.js
@@ -36,8 +36,8 @@ app.config(['$controllerProvider','$compileProvider','$filterProvider','$provide
 
 
 /*Run Phase*/
-app.run(['$rootScope','$state','$stateParams','seven','fns','pushService','$http',
-function( $rootScope , $state , $stateParams , seven , fns , pushService , $http) {
+app.run(['$rootScope','$state','$stateParams','seven','fns','pushService','$http','C',
+function( $rootScope , $state , $stateParams , seven , fns , pushService , $http, C) {
         console.log('Run'); 
         var TpushService = {};
         TpushService.push = function() {
